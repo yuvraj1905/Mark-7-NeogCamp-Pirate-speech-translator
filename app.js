e1fa91a7e15dbc3c06f8f7bb6c53457e4f4ac30b
yuvraj1905 <yuvrajkumar2509@gmail.com>
2022-09-10 17:05:04 +0530

    docs: added css and js files

---
+++ app.js
@@ -0,0 +1,25 @@
+var btnTranslate = document.querySelector("#translate-text-btn");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverUrl = "	https://api.funtranslations.com/translate/pirate.json";
+
+function getTranslationUrl(input) {
+  return serverUrl + "?" + "text=" + input;
+}
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("Something wrong with server , Please try after sometime.");
+}
+
+function clickHandler() {
+  var inputText = txtInput.value;
+  fetch(getTranslationUrl(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
+btnTranslate.addEventListener("click", clickHandler);
